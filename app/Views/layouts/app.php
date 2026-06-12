7adfed222e3bf86300a171eb09585e66f50b608c
spsan <rifsan0710@gmail.com>
2026-06-12 15:39:57 +0700

    fix: undefined \ error in layout

--- app/Views/layouts/app.php
+++ app/Views/layouts/app.php
@@ -12,7 +12,7 @@ $flashInfo    = session()->getFlashdata('info');
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <title><?= $title ? $title . ' — DIANTRA BAKERY' : 'DIANTRA BAKERY ' ?></title>
+    <title><?= ($title ?? '') ? $title . ' — DIANTRA BAKERY' : 'DIANTRA BAKERY' ?></title>
     <link href="<?= base_url('templates/sb-admin/dist/css/styles.css') ?>" rel="stylesheet" />
     <link href="<?= base_url('css/custom.css') ?>" rel="stylesheet" />
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
